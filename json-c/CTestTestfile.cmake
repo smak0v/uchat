3a31a6fa96ec08a6e4a53f6513c86d54877d44fb
Alexander <ezyyk77@gmail.com>
2020-06-11 00:50:30 +0300

    json-c

---
+++ json-c/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/admin/Desktop/json-c
+# Build directory: /Users/admin/Desktop/json-c-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
+subdirs("apps")
+subdirs("doc")
