8ec9a3e6ddbf8b039864587f4d5a25b93783d631
Admin <admin@Mac-Admin.local>
2020-06-10 23:24:54 +0300

    Empty window

--- src/client/init.c
+++ src/client/init.c
@@ -1,21 +1,18 @@
 #include "uchat.h"
 
-static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window = gtk_application_window_new(app);
-
-    gtk_window_set_title(GTK_WINDOW(window), "UCHAT");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-    gtk_widget_show_all(window);
-
-    user_data = NULL;
-}
-
-int mx_init_client(int argc, char **argv) {
+ int mx_init_client(int argc, char **argv) {
     int status = MX_SUCCESS;
-    GtkApplication *app = gtk_application_new("com.ucode.uchat", G_APPLICATION_FLAGS_NONE);
+    GtkWidget *window = NULL;
 
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "UCHAT");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
     return status;
-}
+ }
