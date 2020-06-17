001dc75fd5ebc02010cc44a3d3f7e85833593d2c
Admin <an.ilchuk27@gmail.com>
2020-06-17 23:44:06 +0300

    example login window

--- src/client/init.c
+++ src/client/init.c
@@ -16,14 +16,17 @@ void entry_changed(GtkEntry *s) {
 
 int mx_init_client(int argc, char **argv) {
     GtkBuilder *builder;
-    GObject *win_log;
-    GObject *name_entry;
-    GObject *pass_entry;
-    GObject *button_log_in;
-    GObject *button_reg;
+    GtkWidget *win_log;
+    GtkWidget *name_entry;
+    GtkWidget *pass_entry;
+    GtkWidget *button_log_in;
+    GtkWidget *button_reg;
     GError *error = NULL;
     char *ui_path = mx_build_ui_path("builder.ui");
+    GtkCssProvider *cssProvider = gtk_css_provider_new();
+    // GtkWidget *image = gtk_image_new_from_file (mx_build_ui_path("like.png"));
 
+    
     gtk_init(&argc, &argv);
     builder = gtk_builder_new();
     if (gtk_builder_add_from_file(builder, ui_path, &error) == 0) {
@@ -31,12 +34,17 @@ int mx_init_client(int argc, char **argv) {
         g_clear_error(&error);
         return MX_FAILURE;
     }
+    win_log = GTK_WIDGET(gtk_builder_get_object(builder, "win_log"));
 
-    win_log = gtk_builder_get_object(builder, "win_log");
-    name_entry = gtk_builder_get_object(builder, "name_entry");
-    pass_entry = gtk_builder_get_object(builder, "pass_entry");
-    button_log_in = gtk_builder_get_object(builder, "b_login");
-    button_reg = gtk_builder_get_object(builder, "b_reg");
+    name_entry = GTK_WIDGET(gtk_builder_get_object(builder, "name_entry"));
+    pass_entry = GTK_WIDGET(gtk_builder_get_object(builder, "pass_entry"));
+    button_log_in = GTK_WIDGET(gtk_builder_get_object(builder, "b_login"));
+    button_reg = GTK_WIDGET(gtk_builder_get_object(builder, "b_reg"));
+    gtk_css_provider_load_from_path(cssProvider, mx_build_ui_path("ex.css"), NULL);
+    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
+                                          GTK_STYLE_PROVIDER(cssProvider),
+                                          GTK_STYLE_PROVIDER_PRIORITY_USER);
+
 
     g_signal_connect(win_log, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(name_entry, "changed", G_CALLBACK(entry_changed), NULL);
