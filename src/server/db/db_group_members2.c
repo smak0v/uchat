8b3f345bb7f001309de81b3db960723ce526ef16
Admin <an.ilchuk27@gmail.com>
2020-06-25 18:56:26 +0300

    auditor changes

--- src/server/db/db_group_members2.c
+++ src/server/db/db_group_members2.c
@@ -3,7 +3,8 @@
 int mx_delete_user_from_group(sqlite3 *db, int user_id, int group_id) {
     sqlite3_stmt *stmt = NULL;
     int rv = sqlite3_prepare_v2(db,
-        "DELETE FROM GROUP_MEMBERS WHERE USER_ID = ?1 AND GROUP_ID = ?2", -1, &stmt, NULL);
+        "DELETE FROM GROUP_MEMBERS WHERE USER_ID = ?1"\
+        " AND GROUP_ID = ?2", -1, &stmt, NULL);
 
     sqlite3_bind_int(stmt, 1, user_id);
     sqlite3_bind_int(stmt, 2, group_id);
