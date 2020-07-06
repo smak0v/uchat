fff510034097d52e7bf59e6e4f53e7ac3102decc
ailchuk <an.ilchuk27@gmail.com>
2020-07-06 17:52:40 +0300

    auditor db_load_messages

--- src/server/db/db_get_messages2.c
+++ src/server/db/db_get_messages2.c
@@ -1,33 +1,12 @@
 #include "server.h"
 
-// t_msg *mx_fill_msg(sqlite3_stmt *stmt) {
-//     t_msg *m = malloc(sizeof(t_msg));
-
-//     m->id = sqlite3_column_int(stmt, 0);
-//     m->group_id = sqlite3_column_int(stmt, 1);
-//     m->dialog_id = sqlite3_column_int(stmt, 2);
-//     m->sender = sqlite3_column_int(stmt, 3);
-//     m->msg = strdup((const char*)sqlite3_column_text(stmt, 4));
-//     m->time = sqlite3_column_int(stmt, 5);
-//     m->edited = sqlite3_column_int(stmt, 6);
-//     m->read = sqlite3_column_int(stmt, 7);
-//     if (sqlite3_column_text(stmt, 8) != NULL)
-//         m->file = strdup((const char*)sqlite3_column_text(stmt, 8));
-//     else
-//         m->file = NULL;
-//     if (sqlite3_column_text(stmt, 9) != NULL)
-//         m->forwarded = strdup((const char*)sqlite3_column_text(stmt, 9));
-//     else
-//         m->forwarded = NULL;
-//     return m;
-// }
-
 static t_list *get_group_msg(sqlite3 *db, int group_id, int n) {
     sqlite3_stmt *stmt;
     t_list *group_msg = NULL;
-    
+
     sqlite3_prepare_v2(db, "SELECT * FROM MSG WHERE GROUP_ID = ?1 " \
                        "ORDER BY TIME DESC LIMIT ?2;", -1, &stmt, NULL);
+
     sqlite3_bind_int(stmt, 1, group_id);
     sqlite3_bind_int(stmt, 2, n);
 
@@ -36,6 +15,7 @@ static t_list *get_group_msg(sqlite3 *db, int group_id, int n) {
 
         mx_push_back(&group_msg, tmp);
     }
+
     sqlite3_finalize(stmt);
 
     return group_msg;
@@ -47,6 +27,7 @@ static t_list *get_dialog_msg(sqlite3 *db, int dialog_id, int n) {
 
     sqlite3_prepare_v2(db, "SELECT * FROM MSG WHERE DIALOG_ID = ?1 " \
                        "ORDER BY TIME DESC LIMIT ?2;", -1, &stmt, NULL);
+
     sqlite3_bind_int(stmt, 1, dialog_id);
     sqlite3_bind_int(stmt, 2, n);
 
@@ -55,6 +36,7 @@ static t_list *get_dialog_msg(sqlite3 *db, int dialog_id, int n) {
 
         mx_push_back(&dialog_msg, tmp);
     }
+
     sqlite3_finalize(stmt);
 
     return dialog_msg;
@@ -65,12 +47,12 @@ t_list *mx_db_load_messages(sqlite3 *db, int group_id, int dialog_id, int n) {
 
     if (group_id > 0 && dialog_id < 0) {
         messages = get_group_msg(db, group_id, n);
-        mx_printstr_endl("group********");
     }
+
     if (group_id < 0 && dialog_id > 0) {
         messages = get_dialog_msg(db, dialog_id, n);
-        mx_printstr_endl("dialog********");
     }
+
     return messages;
 }
 
