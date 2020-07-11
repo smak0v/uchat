508021ad3e7cec5d5ddcb2e0457f694f4ac619b8
ailchuk <an.ilchuk27@gmail.com>
2020-07-11 15:06:10 +0300

    change SOCK_FD INT -> SOCK_FD TEXT

--- src/server/db/sock_table.c
+++ src/server/db/sock_table.c
@@ -10,24 +10,25 @@ void mx_new_table_socket(sqlite3 *database) {
 }
 
 int mx_add_sock_user(sqlite3 *db, int user_id,
-                     int sock_fd, char *token) {
+                     char *sock_fd, char *token) {
     sqlite3_stmt *stmt = NULL;
     int rv = 0;
 
     if (mx_get_sock_by_user_id(db, user_id) > 0)
         return -2;
     rv = sqlite3_prepare_v2(db,
-        "INSERT INTO SOCKETS(USER_ID, SOCKET_FD, TOKEN)" \
-        "VALUES(?1, ?2, ?3);", -1, &stmt, NULL);
+        "INSERT INTO SOCKETS(USER_ID, SOCKET_FD, TOKEN)VALUES(?1, ?2, ?3);",
+        -1, &stmt, NULL);
     if (rv == SQLITE_ERROR)
         return -1;
     sqlite3_bind_int(stmt, 1, user_id);
-    sqlite3_bind_int(stmt, 2, sock_fd);
+    sqlite3_bind_text(stmt, 2, sock_fd, -1, SQLITE_STATIC);
     sqlite3_bind_text(stmt, 3, token, -1, SQLITE_STATIC);
-    if ((rv = sqlite3_step(stmt)) != SQLITE_DONE)
+    if ((rv = sqlite3_step(stmt)) != SQLITE_DONE) {
+        sqlite3_finalize(stmt);
         return -1;
+    }
     sqlite3_finalize(stmt);
-
     return 0;
 }
 
@@ -82,7 +83,7 @@ int mx_delete_sock_by_user_id(sqlite3 *db, int user_id) {
         return -1;
 
     rv = sqlite3_prepare_v2(db, "DELETE FROM SOCKETS WHERE USER_ID = ?1;",
-                            -1, &stmt, NULL);
+        -1, &stmt, NULL);
 
     sqlite3_bind_int(stmt, 1, user_id);
 
