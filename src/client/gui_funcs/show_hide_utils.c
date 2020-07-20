e29997ff0435f46c4848eec93d1f30c25e1fecc2
Admin <admin@Mac-Admin.local>
2020-07-20 08:46:56 +0300

    Fixed ifle add call

--- src/client/gui_funcs/show_hide_utils.c
+++ src/client/gui_funcs/show_hide_utils.c
@@ -3,23 +3,23 @@
 gboolean mx_hide_widget(gpointer w) {
     gtk_widget_hide(GTK_WIDGET(w));
 
-    return 1;
+    return G_SOURCE_REMOVE;
 }
 
 gboolean mx_show_widget(gpointer w) {
     gtk_widget_show(GTK_WIDGET(w));
 
-    return 1;
+    return G_SOURCE_REMOVE;
 }
 
 gboolean mx_show_all_widget(gpointer w) {
     gtk_widget_show_all(GTK_WIDGET(w));
 
-    return 1;
+    return G_SOURCE_REMOVE;
 }
 
 gboolean mx_destroy_widget(gpointer w) {
     gtk_widget_destroy(GTK_WIDGET(w));
 
-    return 1;
+    return G_SOURCE_REMOVE;
 }
