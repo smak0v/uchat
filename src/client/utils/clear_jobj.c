c172c40e4fec92774834eed3445b0d99507ffd1f
Sergey Makov <smakov@e1r1p5.unit.ua>
2020-07-26 13:52:42 +0300

    Fixed logout

--- src/client/utils/clear_jobj.c
+++ src/client/utils/clear_jobj.c
@@ -1,8 +1,10 @@
 #include "client.h"
 
 int mx_clear_jobj(json_object **jobj, int status) {
-    free(*jobj);
-    *jobj = NULL;
+    if (*jobj) {
+        free(*jobj);
+        *jobj = NULL;
+    }
 
     return status;
 }
